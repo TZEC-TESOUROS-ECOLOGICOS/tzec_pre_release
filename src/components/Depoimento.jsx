96efcc118f2556a8030807cee80798992feb7d99
Vinícius Schneider <mrschneider.v@gmail.com>
2024-02-07 18:13:06 -0300

    style: add stars rate in depoimentos component

--- src/components/Depoimento.jsx
+++ src/components/Depoimento.jsx
@@ -1,18 +1,33 @@
+import { Star, StarFill } from "react-bootstrap-icons";
 import "../styles/Depoimento.css";
 
 const Depoimento = ({ comment, icon, title, description }) => {
   return (
     <div className="depoimento">
-        <p className="comment">{comment}</p>
+      <p className="comment">{comment}</p>
+      <div className="profile">
         <div className="foto-nomes">
-          <img style={{borderRadius: "100%"}} src={icon} width="60px" alt="" />
+          <img
+            style={{ borderRadius: "100%" }}
+            src={icon}
+            width="60px"
+            alt=""
+          />
           <div className="nome-cargo">
-              <h5 className="nome">{title}</h5>
-              <h6 className="cargo">{description}</h6>
+            <h5 className="nome">{title}</h5>
+            <h6 className="cargo">{description}</h6>
           </div>
         </div>
+        <div className="estrelas">
+          {<StarFill color="yellow" size={20} />}
+          {<StarFill color="yellow" size={20} />}
+          {<StarFill color="yellow" size={20} />}
+          {<StarFill color="yellow" size={20} />}
+          {<Star color="yellow" size={20} />}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Depoimento
+export default Depoimento;
